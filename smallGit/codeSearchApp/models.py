8a251c4aaa44131af3b127f994c6ed4ab9bc0b07
kartikeyrajvaidya <kartikeyrajvaidya@gmail.com>
2018-11-18 15:40:08 +0530

    added models for search History

--- smallGit/codeSearchApp/models.py
+++ smallGit/codeSearchApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SearchHistory(models.Model):
+
+    search_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    search_category = models.CharField(max_length=200)
+    search_query = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.search_query
